95ffd37e4eb7b27ab11d2741511151b9296d8306
Jéferson <jefhtavares@gmail.com>
2016-11-18 21:59:20 -0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SoPt-Gifs
 // @namespace    com.jefhtavares
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://chat.stackexchange.com/rooms/*
@@ -14,6 +14,14 @@
 
     const apiKey = "dc6zaTOxFJmzC"; //Beta API Key - Use only in development
     const apiUrl = "http://api.giphy.com/v1/gifs/search";
+    const modalOptions = {
+        position: { my: "left bottom", at: "left bottom", of: $('#widgets') } ,
+        closeText: '',
+        closeOnEscape: true,
+        draggable: false,
+        title: 'Selecionar GIF',
+        modal: true
+    };
 
     $.getScript("http://code.jquery.com/ui/1.12.0/jquery-ui.min.js", function(){
         $('<link/>', {
@@ -21,10 +29,13 @@
             type: 'text/css',
             href: 'http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css'
         }).appendTo('head');
+
+        $('#widgets').append(htmlInputs());
     });
 
-    $('#widgets').append(htmlInputs());
-    $('#widgets').append(modal());
+    $('.ui-widget-overlay').live('click', function(){
+        $('#dialog').dialog('close');
+    });
 
     $('body').on('click', '#bt-buscar', function () {
         if(!$('#txt-busca').val())
@@ -32,35 +43,32 @@
 
         var url = apiUrl + '?q=' + $('#txt-busca').val() + '&limit=6&api_key=' + apiKey;
         $.get(url, function(data){
-            if(data.data){
-                $('#dialog-content').html('');
-            }
+            var modalSource = modalInicio();
+
             data.data.forEach(function(el){
-                $('#dialog-content').append(montarTagImg(el.images.original.url));
+                var gifUrl = el.images.original.url;
+                modalSource += '<img class="gif" src="' + gifUrl + '" style="width: 120px; height: 80px; cursor: pointer; margin: 3px;" data-url="'+ gifUrl + '"> </img>';
             });
-            
-            $('#dialog').dialog({
-                position: { my: "left bottom", at: "left bottom", of: $('#widgets') } ,
-                closeText: '',
-                closeOnEscape: true,
-                draggable: false,
-                title: 'Selecionar GIF',
-                modal: true
-            });
+
+            modalSource += modalFim();
+
+            $(modalSource).dialog(modalOptions);
         });
     });
 
     $('body').on('click', '.gif', function(){
         $('#input').val($(this).data('url'));
-        //$('#sayit-button').trigger('click');
+        $('#sayit-button').trigger('click');
         $('#dialog').dialog('destroy');
     });
 })();
 
-function modal(){
-    return '<div id="dialog" title="Escolha">' +
-        '<div id="dialog-content"> </div>'+
-        '</div>';
+function modalFim(){
+    return '</div><span>Powered by GIPHY</span></div>';
+}
+
+function modalInicio(){
+    return '<div id="dialog"> <div id="dialog-content">';
 }
 
 function montarTagImg(gifUrl){
