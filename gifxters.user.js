c29c97e35838d5ae68c77434a208de9040588f96
Jéferson <jefhtavares@gmail.com>
2017-02-24 10:30:57 -0300

    Update gifxters.user.js

--- gifxters.user.js
+++ gifxters.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gifxters
 // @namespace    com.jefhtavares
-// @version      0.2
+// @version      0.3
 // @description  Find and send gifs in SE chat
 // @author       https://github.com/jefhtavares
 // @match        http://chat.stackexchange.com/rooms/*
@@ -9,99 +9,108 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function(doc) {
     'use strict';
 
-    String.prototype.replaceAll = function(search, replacement) {
-        var target = this;
-        return target.split(search).join(replacement);
-    };
+    function trigger(){
 
-    var offset = 0, limit = 6;
-    const imgTagTpl = '<img class="gif" src="#GIF-URL#" style="width: 120px; height: 80px; cursor: pointer; margin: 3px;" data-url="#GIF-URL#"> </img>';
-    const apiKey = 'dc6zaTOxFJmzC'; //Beta API Key
-    const apiUrl = 'http://api.giphy.com/v1/gifs/search';
-    const modalOptions = {
-        position: { my: 'left bottom', at: 'left bottom', of: $('#widgets') } ,
-        closeText: '',
-        closeOnEscape: true,
-        draggable: true,
-        resizable: false,
-        title: 'Selecionar GIF',
-        modal: true
-    };
+        String.prototype.replaceAll = function(search, replacement) {
+            var target = this;
+            return target.split(search).join(replacement);
+        };
 
-    $.getScript("http://code.jquery.com/ui/1.12.0/jquery-ui.min.js", function(){
-        $('<link/>', {
-            rel: 'stylesheet',
-            type: 'text/css',
-            href: 'http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css'
-        }).appendTo('head');
+        var offset = 0, limit = 6;
+        const imgTagTpl = '<img class="gif" src="#GIF-URL#" style="width: 120px; height: 80px; cursor: pointer; margin: 3px;" data-url="#GIF-URL#"> </img>';
+        const apiKey = 'dc6zaTOxFJmzC'; //Beta API Key
+        const apiUrl = 'http://api.giphy.com/v1/gifs/search';
+        const modalOptions = {
+            position: { my: 'left bottom', at: 'left bottom', of: $('#widgets') } ,
+            closeText: '',
+            closeOnEscape: true,
+            draggable: true,
+            resizable: false,
+            title: 'Selecionar GIF',
+            modal: true
+        };
 
-        $('#widgets').append(htmlInputs());
-    });
+        $.getScript("http://code.jquery.com/ui/1.12.0/jquery-ui.min.js", function(){
+            $('<link/>', {
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: 'http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css'
+            }).appendTo('head');
 
-    $('.ui-widget-overlay').live('click', function(){
-        $('#dialog').dialog('close');
-    });
+            $('#widgets').append(htmlInputs());
+        });
 
-    $('body').on('click', '#bt-buscar', function () {
-        if(!$('#txt-busca').val())
-            return;
+        $('.ui-widget-overlay').live('click', function(){
+            $('#dialog').dialog('close');
+        });
 
-        offset = 0;
-        var params = $.param({q: $('#txt-busca').val(), limit: 6, offset: offset, api_key: apiKey});
-        var url = apiUrl + '?' + params;
+        $('body').on('click', '#bt-buscar', function () {
+            if(!$('#txt-busca').val())
+                return;
 
-        $.get(url, function(data){
-            var modalSource = modalInicio();
+            offset = 0;
+            var params = $.param({q: $('#txt-busca').val(), limit: 6, offset: offset, api_key: apiKey});
+            var url = apiUrl + '?' + params;
 
-            data.data.forEach(function(el){
-                modalSource += imgTagTpl.replaceAll('#GIF-URL#', el.images.original.url);
+            $.get(url, function(data){
+                var modalSource = modalInicio();
+
+                data.data.forEach(function(el){
+                    modalSource += imgTagTpl.replaceAll('#GIF-URL#', el.images.original.url);
+                });
+
+                modalSource += modalFim();
+
+                $(modalSource).dialog(modalOptions);
+            });
+        });
+
+        $('body').on('click', '#prev-page', function(){
+            offset = offset > 5 ?  offset - 6 : 0;
+            alterar($(this).parent().parent());
+        });
+
+        $('body').on('click', '#next-page', function(){
+            offset += 6;
+            alterar($(this).parent().parent());
+        });
+
+        function alterar(elemento){
+            var params = $.param({q: $('#txt-busca').val(), limit: 6, offset: offset, api_key: apiKey});
+            var url = apiUrl + '?' + params;
+
+            $.get(url, function(data){
+                adicionarGifs(elemento, data.data);
+            });
+        }
+
+        function adicionarGifs(parentElement, data){
+            $(parentElement).find('#dialog-content').html('');
+
+            var strHtml = '';
+            data.forEach(function(el) {
+                strHtml += imgTagTpl.replaceAll('#GIF-URL#', el.images.original.url);
             });
 
-            modalSource += modalFim();
+            $(parentElement).find('#dialog-content').html(strHtml);
+        }
 
-            $(modalSource).dialog(modalOptions);
-        });
-    });
-
-    $('body').on('click', '#prev-page', function(){
-        offset = offset > 5 ?  offset - 6 : 0;
-        alterar($(this).parent().parent());
-    });
-
-    $('body').on('click', '#next-page', function(){
-        offset += 6;
-        alterar($(this).parent().parent());
-    });
-
-    function alterar(elemento){
-        var params = $.param({q: $('#txt-busca').val(), limit: 6, offset: offset, api_key: apiKey});
-        var url = apiUrl + '?' + params;
-
-        $.get(url, function(data){
-            adicionarGifs(elemento, data.data);
+        $('body').on('click', '.gif', function(){
+            $('#input').val($(this).data('url'));
+            $('#sayit-button').trigger('click');
+            $('#dialog').dialog('destroy');
         });
     }
 
-    function adicionarGifs(parentElement, data){
-        $(parentElement).find('#dialog-content').html('');
-
-        var strHtml = '';
-        data.forEach(function(el) {
-            strHtml += imgTagTpl.replaceAll('#GIF-URL#', el.images.original.url);
-        });
-
-        $(parentElement).find('#dialog-content').html(strHtml);
+    if (/^(interactive|complete)$/i.test(doc.readyState)) {
+        trigger();
+    } else {
+        doc.addEventListener('DOMContentLoaded', trigger);
     }
-
-    $('body').on('click', '.gif', function(){
-        $('#input').val($(this).data('url'));
-        $('#sayit-button').trigger('click');
-        $('#dialog').dialog('destroy');
-    });
-})();
+})(document);
 
 function modalFim(){
     return '</div><span>Powered by GIPHY</span><div style="float: right"><button id="prev-page" style="margin-right: 5px;">&#8592;</button><button id="next-page">&#8594;</button></div></div>';
